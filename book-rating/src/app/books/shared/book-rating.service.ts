fe25313ca5174cd7dbd7f01b59c31a92b79edfa5
Ferdinand Malcher <ferdinand@malcher.media>
2020-03-11 11:40:18 +0100

    BookRatingService

--- book-rating/src/app/books/shared/book-rating.service.ts
+++ book-rating/src/app/books/shared/book-rating.service.ts
@@ -9,10 +9,26 @@ export class BookRatingService {
   constructor() { }
 
   rateUp(book: Book): Book {
-    return book;
+    /*if (book.rating < 5) {
+      return {
+        ...book,
+        rating: book.rating + 1
+      };
+    } else {
+      return book;
+    }*/
+
+    return {
+      ...book,
+      rating: book.rating < 5 ? book.rating + 1 : 5
+    };
+
   }
 
   rateDown(book: Book): Book {
-    return null;
+    return {
+      ...book,
+      rating: Math.max(1, book.rating - 1)
+    };
   }
 }
