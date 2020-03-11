afaaa3da64906afdc0eab216173cdb31a2cb9965
Ferdinand Malcher <ferdinand@malcher.media>
2020-03-11 15:04:36 +0100

    EventEmitter testen

--- book-rating/src/app/books/book/book.component.spec.ts
+++ book-rating/src/app/books/book/book.component.spec.ts
@@ -16,10 +16,36 @@ describe('BookComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BookComponent);
     component = fixture.componentInstance;
+
+    component.book = {
+      title: '',
+      isbn: '',
+      description: '',
+      rating: 3
+    };
+
+    // detectChanges erst nach Initialisierung ausführen!
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit event for rateUp', () => {
+
+    let emittedBook;
+
+    // Subscribe auf EventEmitter
+    component.rateUp.subscribe(book => {
+      emittedBook = book;
+    });
+
+    // Methode aufrufen
+    component.onRateUp();
+
+    // Prüfen, ob Event kam
+    expect(emittedBook).toBe(component.book);
+    expect(emittedBook).toBeTruthy();
   });
 });
