ad742eb9ac12233ef98cbbefd10c4d60e824f64d
Ferdinand Malcher <ferdinand@malcher.media>
2020-03-12 17:59:07 +0100

    Fun with Observables

--- book-rating/src/app/app.component.ts
+++ book-rating/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, of, from, timer, interval } from 'rxjs';
 
 @Component({
   selector: 'br-root',
@@ -7,5 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Book Rating';
+
+  constructor() {
+
+    function producer(o) {
+      o.next(1);
+      o.next(2);
+
+      setTimeout(() => o.next(3), 2000);
+      setTimeout(() => o.complete(), 3000);
+    }
+
+    const myObservable$ = new Observable(producer);
+
+
+    const obs = {
+      next: e => console.log(e),
+      error: err => console.error(err),
+      complete: () => console.log('Chemnitz')
+    };
+
+    // producer(obs);
+
+
+    // const myObs$ = interval(500);
+    // myObs$.subscribe(obs);
+
+  }
 
 }
